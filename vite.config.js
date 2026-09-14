fe7913724959c5627d5b2387ec2202c3083e7dfb
prinzipio <lacbawanprince.h@gmail.com>
2026-09-15 04:56:14 +0800

    Use conditional base URL for dev vs production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/jh-listings/',
+  base: process.env.NODE_ENV === 'production' ? '/jh-listings/' : '/',
   plugins: [react()],
 })
